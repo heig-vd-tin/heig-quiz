4a41085b5a18254389759574367ea25980200f85
Yves Chevallier <yves-chevallier@heig-vd.ch>
2020-09-13 14:08:27 +0200

    Add README

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Activity;
+
 class ActivityController extends Controller
 {
-    //
+    function index() {
+        return Activity::where('class.teacher_id', User::id());
+    }
 }
