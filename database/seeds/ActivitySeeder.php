fde272dce66a4256c87853e75c49a5c9720d3e93
MAULAZ Tony <tmz@csem.ch>
2020-09-15 05:17:33 +0000

    Add Activity Seeder

---
+++ database/seeds/ActivitySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('activities')->insert([
+            'quiz_id' => '1',
+            'class_id' => '1',
+            'duration' => '10'
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             //KeywordQuestionSeeder::class
 
             //QuizSeeder::class
+
+            //ActivitySeeder::class
         ]);
     }
 }
