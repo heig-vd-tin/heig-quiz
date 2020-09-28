7b922dbb8c61b18d753eb8645a8283c2ad72696a
Yves Chevallier <nowox@x0x.ch>
2020-09-28 22:32:39 +0200

    Disable Autologin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\AutoLogin::class,
+            // \App\Http\Middleware\AutoLogin::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
 
-            \App\Http\Middleware\AutoLogin::class,
+            // \App\Http\Middleware\AutoLogin::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
