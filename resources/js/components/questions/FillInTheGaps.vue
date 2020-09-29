f13d37f0cd423813fa47212f6f155a27ea16b26f
Yves Chevallier <nowox@x0x.ch>
2020-09-29 11:43:24 +0200

    Working on fill-in-the-gaps

--- resources/js/components/questions/FillInTheGaps.vue
+++ resources/js/components/questions/FillInTheGaps.vue
@@ -2,18 +2,55 @@
 Allow to give a short answer (one line of text)
 -->
 <template>
-  <div>
-    <markdown-it-vue :content="content" />
-
-  </div>
+  <b-form inline ref="content">
+      <markdown-it-vue :content="content" />
+  </b-form>
 </template>
 <script>
+import GapComponent from "./gap";
+import Vue from "vue";
+
+import MarkdownItVue from "markdown-it-vue";
+import "markdown-it-vue/dist/markdown-it-vue.css";
+
+let MyMarkdownItVue = Vue.extend(MarkdownItVue);
+let Gap = Vue.extend(GapComponent);
+
 export default {
+  components: {
+    Gap,
+  },
   props: {
     content: String, // Question Markdown content
     gaps: Object, // Gaps type
     answered: Object,
     is_correct: { type: Boolean, default: null },
   },
+  mounted() {
+    let md = new MyMarkdownItVue({ propsData: { content: this.content } });
+    md.$mount();
+    this.$nextTick().then(() => {
+      //console.log(md.$el);
+      //console.log(md.$el.querySelectorAll("code"));
+      this.$refs.content.querySelectorAll("code").forEach((code, index) => {
+        console.log(code);
+        let gap = new Gap({
+          propsData: {
+            name: "Foobar",
+            options: ["a", "b", "c"],
+          },
+        });
+        gap.$mount();
+        code.replaceWith(gap.$el);
+      });
+
+    });
+  },
+  updated() {},
 };
 </script>
+<!--
+Remplacer les `gap` par un composant `<gap :name="gap.name" :options="gap.option"/>`
+
+Soit chercher tous les `codes` dans un truc
+-->
---
+++ resources/js/components/questions/gap.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+      <b-form-text class="display:inline;" v-if="options.length == 0"></b-form-text>
+      <b-form-select class="display:inline;"  v-else>
+        <b-form-select-option v-for="(option, index) in options" :key="index" :value="value">
+          {{ option }}
+        </b-form-select-option>
+      </b-form-select>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    name: String,
+    options: {
+      type: Array,
+      default: []
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
