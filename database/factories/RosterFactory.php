74abd2e44bd6962b2bede197d4e45e2f7160b83e
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-02-19 14:17:38 +0000

    Get log info from aai

---
+++ database/factories/RosterFactory.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+use App\Models\Roster;
+use App\Models\User;
+
+class RosterFactory extends Factory
+{
+    protected $model = Roster::class;
+
+    public function definition()
+    {
+        $teachers_id = User::where('affiliation', 'member;staff')->pluck('id')->toArray();
+        return [
+            'name' => Arr::random(['A', 'B', 'C']),
+            'semester' => Arr::random([0, 0, 0, 0, 0, 1]),
+            'year' => Arr::random([2018, 2019, 2020, 2020, 2020, 2020, 2020, 2020, 2021]),
+            'teacher_id' => Arr::random($teachers_id),
+        ];
+    }
+}
