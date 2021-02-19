b8ef31d9627a1e1b969a94a9bfcab0d58a74103f
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-02-19 14:17:38 +0000

    Get log info from aai

---
+++ app/Http/Middleware/AutoLogin.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Closure;
+use Auth;
+
+class AutoLogin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $identity = config('devel.autologin');
+
+	$surname = $_SERVER['HTTP_SURNAME'];
+	$name = $_SERVER['HTTP_GIVENNAME'];
+	$email = $_SERVER['HTTP_MAIL'];
+	$aff = $_SERVER['HTTP_AFFILIATION'];
+	$uid = $_SERVER['HTTP_UNIQUEID'];
+	$full = $surname . " " . $name;
+
+	$u = User::where('unique_id', $uid)->first();
+	if($u == null){
+		$u = new User();
+		$u->unique_id = $uid;
+		$u->firstname = $name;
+		$u->lastname = $surname;
+		$u->email = $email;
+		$u->name = $full;
+		$u->affiliation = $aff;
+		$u->save();
+	}
+
+	if(!Auth::check()){
+		Auth::loginUsingId($u->id);
+	}
+
+        return $next($request);
+    }
+}
