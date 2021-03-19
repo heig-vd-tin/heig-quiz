5b71eada66a9c3334285a61f19bb85459d7a0cba
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-03-19 15:02:48 +0000

    Add autologin id for test

--- app/Http/Middleware/AutoLogin.php
+++ app/Http/Middleware/AutoLogin.php
@@ -13,34 +13,40 @@ class AutoLogin
     {
         $identity = config('devel.autologin');
 
-	$surname = $_SERVER['HTTP_SURNAME'];
-	$name = $_SERVER['HTTP_GIVENNAME'];
-	$email = $_SERVER['HTTP_MAIL'];
-	$aff = $_SERVER['HTTP_AFFILIATION'];
-	$uid = $_SERVER['HTTP_UNIQUEID'];
-	$full = $surname . " " . $name;
-
-	$u = User::where('unique_id', $uid)->first();
-	if($u == null){
-		$u = new User();
-		$u->unique_id = $uid;
-		$u->firstname = $name;
-		$u->lastname = $surname;
-		$u->email = $email;
-		$u->name = $full;
-		$u->affiliation = $aff;
-		$u->save();
-
-		if( str_contains($aff, 'student') ){
-			$s = new Student();
-			$s->user_id = $u->id;
-			$s->save();	
-		}	
-	}
-
-	if(!Auth::check()){
-		Auth::loginUsingId($u->id);
-	}
+		if( array_key_exists('HTTP_SURNAME', $_SERVER) )
+		{
+			$surname = $_SERVER['HTTP_SURNAME'];
+			$name = $_SERVER['HTTP_GIVENNAME'];
+			$email = $_SERVER['HTTP_MAIL'];
+			$aff = $_SERVER['HTTP_AFFILIATION'];
+			$uid = $_SERVER['HTTP_UNIQUEID'];
+			$full = $surname . " " . $name;
+		
+			$u = User::where('unique_id', $uid)->first();
+			if($u == null){
+				$u = new User();
+				$u->unique_id = $uid;
+				$u->firstname = $name;
+				$u->lastname = $surname;
+				$u->email = $email;
+				$u->name = $full;
+				$u->affiliation = $aff;
+				$u->save();
+		
+				if( str_contains($aff, 'student') ){
+					$s = new Student();
+					$s->user_id = $u->id;
+					$s->save();	
+				}	
+			}
+			
+			if(!Auth::check()){
+				Auth::loginUsingId($u->id);
+			}
+		}
+		else{
+			Auth::loginUsingId(config('devel.autologinid'));
+		}
 
         return $next($request);
     }
